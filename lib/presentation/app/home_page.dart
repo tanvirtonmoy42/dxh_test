9f8d528bca62007b6e7b407f5967605a8facf81b
tanvirtonmoy42 <48643228+tanvirtonmoy42@users.noreply.github.com>
2024-03-04 17:38:25 +0600

    search

--- lib/presentation/app/home_page.dart
+++ lib/presentation/app/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:dxh_test/application/app/product_provider.dart';
 import 'package:dxh_test/presentation/app/cart/cart_screen.dart';
 import 'package:dxh_test/presentation/app/product/widgets/product_cart.dart';
+import 'package:dxh_test/presentation/common_widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
@@ -10,7 +12,8 @@ class Homepage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    final products = ref.watch(productListProvider(''));
+    final searchController = useTextEditingController();
+    final products = ref.watch(productListProvider(searchController.text));
     return Scaffold(
       appBar: AppBar(
         title: const Text('Homepage'),
@@ -25,21 +28,49 @@ class Homepage extends HookConsumerWidget {
           SizedBox(width: 2.w),
         ],
       ),
-      body: products.maybeWhen(
-        data: (data) => GridView.builder(
-          padding: EdgeInsets.symmetric(horizontal: 5.w),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 5.w,
-            mainAxisSpacing: 5.w,
-            childAspectRatio: .72,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5.w),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 1.h),
+              CustomTextField(
+                controller: searchController,
+                hintText: 'Search Here...',
+                suffixIcon: InkWell(
+                  onTap: () {
+                    ref.invalidate(productListProvider);
+                  },
+                  child: Icon(
+                    Icons.search_rounded,
+                    size: 23.sp,
+                  ),
+                ),
+                onChanged: (value) {
+                  ref.invalidate(productListProvider);
+                },
+              ),
+              SizedBox(height: 2.h),
+              products.maybeWhen(
+                data: (data) => GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 5.w,
+                    mainAxisSpacing: 5.w,
+                    childAspectRatio: .72,
+                  ),
+                  itemCount: data.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) =>
+                      ProductCart(product: data[index]),
+                ),
+                orElse: () => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            ],
           ),
-          itemCount: data.length,
-          shrinkWrap: true,
-          itemBuilder: (context, index) => ProductCart(product: data[index]),
-        ),
-        orElse: () => const Center(
-          child: CircularProgressIndicator(),
         ),
       ),
     );
